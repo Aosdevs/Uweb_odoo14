ba70a7c53a1e5879d99041ef9a42daf6866f17a7
Isaac Rodrigues Silva Homem de Mello <54726445+Isaachintosh@users.noreply.github.com>
2024-08-10 18:11:02 -0300

    python3.8

---
+++ 14.0/install_python3.8.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# install PPA
+add-apt-repository ppa:deadsnakes/ppa -y
+
+# update and install
+apt update
+apt install python3.8 python3.8-dev python3.8-venv
+
+# setup alternatives
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3 1
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2
+
+# show menu for selecting the version
+update-alternatives --config python3
+
+# or one command to set it
+# update-alternatives --set python3 /usr/bin/python3.8
